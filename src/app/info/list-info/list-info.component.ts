7c1b8562fc98f62ba41c3dfe3c1aefcd09340519
szentesik <sz.krisz@freemail.hu>
2024-08-09 17:41:32 +0200

    App skeleton & material

---
+++ src/app/info/list-info/list-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-info.component.html',
+  styleUrl: './list-info.component.scss'
+})
+export class ListInfoComponent {
+
+}
